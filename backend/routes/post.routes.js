c48afe115e4823cf57202ecbbae03b538fc24666
hrishike-sh <rushisalokhe456@gmail.com>
2024-10-04 19:41:20 +0530

    POSTS

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -10,16 +10,20 @@ import {
   getPosts,
   getLikes,
   getFollowingPosts,
+  getUserPosts,
 } from "../controllers/post.controller.js";
 const router = express.Router();
 
-router.get("/all", protectRoute, getPosts);
-router.get("/following", protectRoute, getFollowingPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentPost);
+
 router.delete("/comment/:postId/:id", protectRoute, deleteComment);
 router.delete("/:id", protectRoute, deletePost);
+
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/all", protectRoute, getPosts);
 router.get("/likes/:id", protectRoute, getLikes);
+router.get("/user/:id", protectRoute, getUserPosts);
 
 export default router;
