f0f202f649093606a6af40ea5525f01a3b48f54c
hrishike-sh <rushisalokhe456@gmail.com>
2024-10-06 23:04:29 +0530

    auth handled & routes added to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,73 @@
-import { useState } from 'react'
-import {Routes, Route} from 'react-router-dom'
-import HomePage from './pages/home/HomePage';
-import LoginPage from './pages/auth/login/LoginPage';
-import SignUpPage from './pages/auth/signup/SignUpPage';
-import Sidebar from './components/common/Sidebar';
-import RightPanel from './components/common/RightPanel';
-import NotificationPage from './pages/notification/NotificationPage';
-import ProfilePage from './pages/profile/ProfilePage';
+import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 function App() {
+  const { data: authUser, isLoading } = useQuery(
+    {
+      queryKey: ["authUser"],
+      queryFn: async () => {
+        try {
+          const res = await fetch("/api/auth/me");
+          const data = await res.json();
+          if (!res.ok) {
+            throw new Error(data.message || data.error);
+          }
+          return data;
+        } catch (e) {}
+      }
+    },
+    {
+      retry: false,
+      retryOnMount: false
+    }
+  );
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
 
   return (
-    <div className='flex max-w-6xl mx-auto'>
-      <Sidebar />
+    <div className="flex max-w-6xl mx-auto">
+      {authUser ? <Sidebar /> : null}
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/notifications' element={<NotificationPage />} />
-        <Route path='/profile/:username' element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <LoginPage />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUpPage />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
-      <RightPanel />
+      {authUser ? <RightPanel /> : null}
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
