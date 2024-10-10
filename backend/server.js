bd8f7ebcbe33970b6dc741e72f666b79bb36a787
hrishike-sh <rushisalokhe456@gmail.com>
2024-10-10 23:01:47 +0530

    create post

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,10 @@ import connectMongoDB from "./db/connectMongoDB.js";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
